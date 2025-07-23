3e4ac26e3cbfeb75eb7e46185afe9ea09dbf2742
Muhammed Nizam Ali <nizamali985@gmail.com>
2025-07-23 23:59:36 +0530

    2-D array in python

--- Python Tour Begins/array.py
+++ Python Tour Begins/array.py
@@ -13,3 +13,20 @@ arr.remove(3)
 # Iterating through list
 for item in arr:
     print(item)
+print("2-D array")
+# 2D Array (Matrix) using a list of lists
+matrix = [
+    [1, 2, 3],
+    [4, 5, 6],
+    [7, 8, 9]
+]
+
+# Accessing elements (row 1, col 2)
+print(matrix[0][1])  # Output: 2
+
+# Iterating through 2D array
+for row in matrix:
+    for col in row:
+        print(col, end=' ')
+    print()
+
