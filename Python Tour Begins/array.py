4b88465d95f1a3af6e4c063c4c781748b82c1020
Muhammed Nizam Ali <nizamali985@gmail.com>
2025-07-23 23:56:46 +0530

    1-D array in python

---
+++ Python Tour Begins/array.py
@@ -0,0 +1,15 @@
+# Creating a list (array-like)
+arr = [1, 2, 3, 4, 5]
+
+# Accessing elements
+print(arr[0])  # Output: 1
+
+# Adding elements
+arr.append(6)
+
+# Removing elements
+arr.remove(3)
+
+# Iterating through list
+for item in arr:
+    print(item)
