28d56284deae324831df5db1e3ea71eedc144577
Muhammed Nizam Ali <nizamali985@gmail.com>
2025-08-03 23:59:27 +0530

    str learn in ython

---
+++ Python Tour Begins/stringTourPython.py
@@ -0,0 +1,11 @@
+# String Slicing
+
+str = "GeeksforGeeks"
+print(str[0:4])  # Output: Geeks
+print(str[5:])    # Output: forGeeks
+
+#str[0] = 'I'
+#print(str)  # This will raise an error because strings are immutable in Python
+
+str = "I am a geek" + " and I love coding"
+print(str)  # Output: I am a geek and I love coding
