f8b0b5bd1301f47bf4b8da4d0596e3b88857b32e
Muhammed Nizam Ali <nizamali985@gmail.com>
2025-08-04 23:30:18 +0530

    string practice in python

--- Python Tour Begins/stringTourPython.py
+++ Python Tour Begins/stringTourPython.py
@@ -9,3 +9,43 @@ print(str[5:])    # Output: forGeeks
 
 str = "I am a geek" + " and I love coding"
 print(str)  # Output: I am a geek and I love coding
+print(len(str))  # Output: 30
+print(str.upper())
+print(str.lower())
+
+#strip 
+strStrip = "   Hello, World!   "
+print(strStrip.strip())  # Output: Hello, World!
+
+strReplace = "Hi, I am Tom"
+print(strReplace.replace("Tom", "Nizam"))
+
+# COncatanation and Repetitive Strings
+str1 = "Hello"
+str2 = "World"
+print(str1 + " " + str2)  # Output: Hello World
+print(str1 * 3)  # Output: HelloHelloHello
+
+# Formatting Strings
+name = "Alice"
+age = 30
+print(f"My name is {name} and I am {age} years old.")
+
+# String Methods
+str = "Python Programming"
+print("Python" in str)
+print("GfG" in str)
+
+s = "Hello, Python!"
+
+print(s[0:5])  # Output: Hello
+print(s[7:])    # Output: Python!
+print(s[:-5])  # Output: Hell
+
+s = "abcdefghijklmnopqrstuvwxyz"
+print(s[::2])  # Output: acegikmoqsuwy
+print(s[-4:]) 
+print(s[:-3])
+print(s[-5:-2])  # Output: vwx
+print(s[-8:-1:3])
+print(s[::-1])
