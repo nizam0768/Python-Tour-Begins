e3071169e269d05f16922d35cfd622f4d60e54e4
Muhammed Nizam Ali <nizamali985@gmail.com>
2025-08-08 23:26:55 +0530

    lerning python string function

---
+++ Python Tour Begins/stringFunction.py
@@ -0,0 +1,16 @@
+#string function in python
+sentence1 = "My Name is Khan. Embarking on a new journey."
+sentence2 = ['I', 'am', 'a', 'software', 'engineer.']
+capatilized_sentence1 = sentence1.capitalize()
+print(' '.join(sentence2))  # Output: I am a software engineer. My Name is Khan. Embarking on a new journey.
+print(capatilized_sentence1)  # Output: My name is khan.
+print(sentence1.upper())  # Output: MY NAME IS KHAN.
+print(sentence1.lower())  # Output: my name is khan. embarking on a new journey.
+print(sentence1.title())  # Output: My Name Is Khan. Embarking On A New Journey.
+print(sentence1.swapcase())  # Output: mY nAME IS kHAN. eMBARKING ON A NEW JOURNEY.
+print(sentence1.replace("Khan", "Khan Academy"))  # Output: My Name is Khan Academy. Embarking on a new journey.
+print(sentence1.split())  # Output: ['My', 'Name', 'is', 'Khan.', 'Embarking', 'on', 'a', 'new', 'journey.']
+print(sentence1.split(" "))  # Output: ['My', 'Name', 'is', 'Khan.', 'Embarking', 'on', 'a', 'new', 'journey.']
+print(sentence1.split("a"))  # Output: ['My N', 'me is Khan. Emb', 'rking on ', ' new journey.']
+print(sentence2.count("e"))  # Output: 5
+#print(sentence2.find("software"))  # Output: 10
