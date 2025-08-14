0dc7c2f418e8d206433880179f10cabca9b5c81d
Muhammed Nizam Ali <nizamali985@gmail.com>
2025-08-14 23:34:00 +0530

    another we to find missing element in python

--- Python Tour Begins/FindMissingElement.py
+++ Python Tour Begins/FindMissingElement.py
@@ -7,4 +7,17 @@ def MissingElement(arr):
             while(diff < arr[i] - i):
                 print("Missing element is: ", i + diff)
                 diff += 1
-MissingElement([1, 2, 3, 4, 6, 9, 13, 15])  # Output: Missing element is: 5
+MissingElement([1, 2, 3, 4, 6, 9, 13, 15]) 
+print("--------------Another example using Hasing Technique--------------")
+# Another example
+def MissingElementUsingHassing(arr):
+    maxElement = max(arr)
+    array = [0] * (maxElement+1)
+    for num in arr:
+        array[num] += 1
+    for i in range(1, len(array)):
+        if(array[i] == 0):
+            print("Missing element is: ", i)
+
+MissingElementUsingHassing([1, 2, 3, 4, 6, 9, 13, 15])
+
