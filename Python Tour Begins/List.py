b63ca2a8a1dc5665aafe44a3829c4adb112e12b8
Muhammed Nizam Ali <nizamali985@gmail.com>
2025-09-10 21:49:19 +0530

    learning about list

--- Python Tour Begins/List.py
+++ Python Tour Begins/List.py
@@ -65,3 +65,9 @@ for element in a:
     c += 1
 
 print(c)
+
+sentence = "the quick brown fox jumps over the lazy dog"
+words = sentence.split()
+word_lengths = [len(word) for word in words if word != "the"]
+print(words)
+print(word_lengths)
